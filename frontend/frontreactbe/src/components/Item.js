fe123be30df16f368701bb45fe458886ab66ac1e
Lucía Krayacich <luciakrayacich@gmail.com>
2021-09-05 03:46:17 -0300

    Front para filtros en vista de productos.

--- frontend/frontreactbe/src/components/Item.js
+++ frontend/frontreactbe/src/components/Item.js
@@ -7,7 +7,9 @@ const Item = ({item})=> {
     return (
         <div className="item">
             <img src={item.thumbnail} alt={item.name} height="70px" width="70px"></img>
-            <p className="itemP">{item.name} ${item.price}</p>
+            <p className="itemP">{item.name}</p>
+            <p className="itemP">${item.price}</p>
+            <p className="itemP">Código: {item.code}</p>
             <div>
                 <NavLink to={`/${item._id}`}>Ver detalle</NavLink>
             </div>
--- frontend/frontreactbe/src/components/ItemListContainer.js
+++ frontend/frontreactbe/src/components/ItemListContainer.js
@@ -5,6 +5,8 @@ import '../styles.css'
 
 const ItemListContainer = ({greeting})=>{
     const [items, setItems] = useState({})
+    const [prodName, setProdName] = useState()
+    const [prodCode, setProdCode] = useState()
     const {categoryId} = useParams()
 
     useEffect(()=>{
@@ -29,9 +31,42 @@ const ItemListContainer = ({greeting})=>{
         setItems(result)    
     }
 
+    const getByName = async (name)=>{
+        const response = await fetch(`http://localhost:9000/productos/listar/nombre/${name}`)
+        const result = await response.json()
+        setItems(result)
+    }
+    const getByCode = async (code)=>{
+        const response = await fetch(`http://localhost:9000/productos/listar/codigo/${code}`)
+        const result = await response.json()
+        setItems(result)
+    }
+    const getByStock = async ()=>{
+        const response = await fetch(`http://localhost:9000/productos/listar/stock`)
+        const result = await response.json()
+        setItems(result)
+    }
+    const getPriceUnder = async ()=>{
+        const response = await fetch(`http://localhost:9000/productos/listar/precio/menor`)
+        const result = await response.json()
+        setItems(result)
+    }
+    const getPriceOver = async ()=>{
+        const response = await fetch(`http://localhost:9000/productos/listar/precio/mayor`)
+        const result = await response.json()
+        setItems(result)
+    }
+
     return (
         <>
             <h1>{greeting}</h1>
+            <input type="text" id="prodName" name="prodName" onInput={e=>setProdName(e.target.value)} placeholder="Nombre del producto"/>
+            <button onClick={()=>{getByName(prodName)}}>Buscar</button><br/>
+            <input type="text" id="prodCode" name="prodCode" onInput={e=>setProdCode(e.target.value)} placeholder="código"/>
+            <button onClick={()=>{getByCode(prodCode)}}>Buscar</button><br/>
+            <button onClick={getByStock}>Filtrar por disponible</button><br/>
+            <button onClick={getPriceUnder}>Filtrar por precio menor a $60</button><br/>
+            <button onClick={getPriceOver}>Filtrar por precio de $60 para arriba</button><br/>
             <ItemList items={items}/>
         </>
     )
