60cd783752fc5022b54c4cbf25a6c2b5e22b6d58
Lucía Krayacich <luciakrayacich@gmail.com>
2021-11-07 23:38:33 -0300

    Instalación de dependencias; Setup de estructura y server para trabajar.

--- backend/src/config/globals.js
+++ backend/src/config/globals.js
@@ -2,6 +2,12 @@ require('dotenv').config()
 
 module.exports = {
     PORT: process.env.PORT || 8080,
-    NODE_ENV: process.env.NODE_ENV,
+    NODE_ENV: process.env.NODE_ENV || "development",
     MONGO_URI: process.env.MONGO_URI || "",
+    SESSION_SECRET: process.env.SESSION_SECRET || "secret",
+    TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
+    TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
+    PASS_GMAIL: process.env.PASS_GMAIL,
+    USER_GMAIL: process.env.USER_GMAIL,
+    ADMIN_PHONE: process.env.ADMIN_PHONE
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,5 @@
+const { SESSION_SECRET, MONGO_URI } = require('./config/globals')
+
 //CONFIG DEL SERVIDOR
 const http = require('http')
 const express = require('express')
@@ -8,14 +10,40 @@ const routerApi = express.Router()
 const routerProductos = express.Router()
 const routerCarrito = express.Router()
 
+//db
+const MongoStore = require('connect-mongo')
+const mongoOptions = {useNewUrlParser: true, useUnifiedTopology: true}
+
+//Authentication
+const passport = require('passport')
+const session = require("express-session")
+require('./src/auth/passportLocal')
+
 //Require middlewares
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const compression = require('compression')
 
+//Middleware session
+app.use(
+    session({
+        store: MongoStore.create({
+            mongoUrl: MONGO_URI,
+            mongoOptions,
+        }),
+        secret: SESSION_SECRET,
+        resave: true,
+        saveUninitialized: true,
+        cookie: {
+            maxAge: 600 * 1000
+        }
+    })
+)
 //Setting middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(cookieParser())
 app.use(cors())
 app.use(compression())
